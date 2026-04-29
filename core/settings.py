177731c11ebce546cb0601a482cad54d22dd86d5
goar-coder <goaroruesanchez@gmail.com>
2026-04-29 09:13:50 +0200

    puerto para el mysql

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Aquí puedes añadir tus apps locales
 ]
 
 MIDDLEWARE = [
